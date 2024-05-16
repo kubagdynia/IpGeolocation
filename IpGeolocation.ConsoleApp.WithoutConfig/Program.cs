43e884d3a34fa61ec7e1da5b7c7cf32b3d598f55
kubagdynia <kubagdynia@gmail.com>
2024-05-16 15:39:07 +0200

    Added ConsoleApp.WithoutConfig

---
+++ IpGeolocation.ConsoleApp.WithoutConfig/Program.cs
@@ -0,0 +1,24 @@
+﻿using IpGeolocation.Extensions;
+using IpGeolocation.Models;
+using IpGeolocation.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var services = new ServiceCollection();
+
+services.UseIpGeolocation(settings: null);
+services.AddTransient<App>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var ipGeolocation = await serviceProvider.GetService<App>()!.GetIpGeolocationAsync("8.8.8.8");
+
+Console.WriteLine(ipGeolocation.CountryName);
+
+serviceProvider.Dispose();
+
+internal class App(IIpGeolocationService ipGeolocationService)
+{
+    public async Task<IpGeolocationModel> GetIpGeolocationAsync(string ipAddress)
+        => await ipGeolocationService.GetIpGeolocationAsync(ipAddress);
+}
